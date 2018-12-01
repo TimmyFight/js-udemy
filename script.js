a7a0bb75e973976b89fbc4fd42c62b1792ebfa86
TimmyFight <gtaaa@wp.pl>
2018-12-01 21:02:00 +0100

    Sekcja 5 Wykład 18

--- script.js
+++ script.js
@@ -8,3 +8,53 @@ var a = 1, b = 2, c = 3;
 y = 10;
 
 alert("test " + imie + " Masz " + (x+y));
+
+if(a<b) {
+    alert("a jest mniejsze od b");
+} else if (a>b) {
+    alert("a jest większe od b");
+} else {
+    alert("a nie jest ani większe ani mniejsze od b");
+}
+
+(x%2==0) ? alert("Zmeinna X= " + x + "jet parzyta") :  alert("Zmeinna X= " + x + " jet nie parzyta");
+
+var dzienTygodnia = 6;
+switch (dzienTygodnia)
+{
+    case 1: 
+    alert("Dzisiaj jest poniedziałek");
+    break;
+
+    case 2: 
+    alert("Dzisiaj jest wtorek");
+    break;
+
+    case 6:
+    case 7:
+    alert("Weekend!!!!");
+    break
+
+    default: 
+    alert("Nie ma takiego dnia tygodnia");
+
+}
+
+function test(x, y) {
+
+    if (y===0) {
+        alert("Nie dziel przez 0");
+        return;
+    }
+    return x/y;
+}
+
+var wynikFunkcji = test(2,0);
+alert("Wynik działania Funkcji TEST to: " + wynikFunkcji);
+
+
+var zmiennaFunkcji = function() {
+   
+}
+
+zmiennaFunkcji();
